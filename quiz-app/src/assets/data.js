8286c342a7656a43d57c1c3158b2e936ec3f9b6a
Malika <malikaoubilla@gmail.com>
2024-05-31 21:18:16 +0100

    quiz complete

---
+++ quiz-app/src/assets/data.js
@@ -0,0 +1,42 @@
+export const data = [
+  {
+    question: "How many time zones are there in Russia?",
+    option1: "11",
+    option2: "1",
+    option3: "5",
+    option4: "8",
+    answer: 1,
+  },
+  {
+    question: "What country has the most islands in the world?",
+    option1: "Sweden",
+    option2: "Indonesia",
+    option3: "Philippines",
+    option4: "Canada",
+    answer: 1,
+  },
+  {
+    question: "How many stripes are there on the US flag?",
+    option1: "10",
+    option2: "12",
+    option3: "13",
+    option4: "15",
+    answer: 3,
+  },
+  {
+    question: "When was Netflix founded?",
+    option1: "1997",
+    option2: "2001",
+    option3: "2009",
+    option4: "2015",
+    answer: 1,
+  },
+  {
+    question: "Name Disney's first film?",
+    option1: "Cinderella",
+    option2: "Sleeping Beauty",
+    option3: "Snow White",
+    option4: "Pinocchio",
+    answer: 3,
+  },
+];
--- quiz-app/src/components/Quiz.jsx
+++ quiz-app/src/components/Quiz.jsx
@@ -1,18 +1,77 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import './Quiz.css'
+import { data } from '../assets/data';
+
 
 function Quiz() {
+  let [index, setIndex] = useState(0);
+  let [question, setQuestion] = useState(data[index]);
+  let [selectedOption, setSelectedOption] = useState(null);
+  let [score, setScore] = useState(0);
+
+  useEffect(() => {
+    // Disable the "Next question" button on the first render
+    document.getElementById('next-question-button').disabled = true;
+  }, []);
+
+  function showScore() {
+    const quizContainer = document.querySelector('.quiz-container');
+    quizContainer.innerHTML = '';
+    const scoreMessage = document.createElement('h2');
+    scoreMessage.textContent = `Your score is ${score} out of ${data.length}`;
+    quizContainer.appendChild(scoreMessage);
+  }
+
+  function selector(e) {
+    const allOptions = document.querySelectorAll('.quiz-container li');
+    allOptions.forEach(option => option.classList.remove('selected'));
+    e.target.classList.add('selected');
+    setSelectedOption(e.target.id);
+  }
+  function checkAnswer() {
+    const e = document.getElementById(selectedOption);
+    e.classList.remove('selected');
+    if (question.answer == selectedOption) {
+      e.classList.add("correct");
+      setScore(++score);
+    } else {
+      e.classList.add("wrong");
+    }
+    document.getElementById('next-question-button').disabled = false;
+    document.getElementById('check-answer-button').disabled = true;
+  }
+  function nextQuestion() {
+    if (index === data.length-1) {
+      showScore();
+    } else {
+      setIndex(++index);
+      setQuestion(data[index]);
+      if (index === data.length-1) {
+        document.getElementById('next-question-button').textContent = "Done";
+      }
+      setSelectedOption(null);
+      document.querySelectorAll('.quiz-container li').forEach(option => {
+        option.classList.remove('correct', 'wrong', 'selected');
+      });
+      document.getElementById('next-question-button').disabled = true;
+      document.getElementById('check-answer-button').disabled = false;
+    }
+  }
+
   return (
     <div className='quiz-container'>
-      <h2>How many time zones are there in Russia?</h2>
+      <h2>{index + 1}. {question.question}</h2>
       <ul>
-        <li>11</li>
-        <li>1</li>
-        <li>5</li>
-        <li>8</li>
+        <li id="1" onClick={selector}>{question.option1}</li>
+        <li id="2" onClick={selector}>{question.option2}</li>
+        <li id="3" onClick={selector}>{question.option3}</li>
+        <li id="4" onClick={selector}>{question.option4}</li>
       </ul>
-      <button>Next question</button>
-      <div className='index'>1 of 5 questions</div>
+      <div class="button-container">
+        <button id="check-answer-button" onClick={checkAnswer}>Check answer</button>
+        <button id="next-question-button" onClick={nextQuestion}>Next question</button>
+      </div>
+      <div className='index'>{index + 1} of {data.length} questions</div>
     </div>
   )
 }
