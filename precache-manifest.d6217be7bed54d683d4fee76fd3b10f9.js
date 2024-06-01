12be6453711fd1cb5f3c6b992e593ce7249530b8
Malika <malikaoubilla@gmail.com>
2024-06-01 16:43:46 +0100

    Updates

---
+++ precache-manifest.d6217be7bed54d683d4fee76fd3b10f9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "164178f107dafbf84ec017e1ff07d6dd",
+    "url": "/React-apps/index.html"
+  },
+  {
+    "revision": "2be96ef391f89506e5ac",
+    "url": "/React-apps/static/css/main.3930e9ef.chunk.css"
+  },
+  {
+    "revision": "4e573ae7a059b092691f",
+    "url": "/React-apps/static/js/2.37b9e00f.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/React-apps/static/js/2.37b9e00f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2be96ef391f89506e5ac",
+    "url": "/React-apps/static/js/main.dd376b09.chunk.js"
+  },
+  {
+    "revision": "c0ee98eac5fc9756857d",
+    "url": "/React-apps/static/js/runtime-main.dff958a7.js"
+  }
+]);
